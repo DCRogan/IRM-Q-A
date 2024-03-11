af1dec8a265c1e77619e8915b7256f10f8e2ac18
Rogan Rong <rogan.rong@sap.com>
2024-03-11 16:21:56 +0800

    fix issue

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain.chat_models import AzureChatOpenAI
 
 @st.cache_resource
 def load_data(vector_store_dir: str="faiss"):
-    db = FAISS.load_local(vector_store_dir, AzureOpenAIEmbeddings())
+    db = FAISS.load_local(vector_store_dir, AzureOpenAIEmbeddings(), allow_dangerous_deserialization=True)
     llm = AzureChatOpenAI(model_name="gpt-35-turbo", temperature=0.5)
 
     print("Loading data...")
